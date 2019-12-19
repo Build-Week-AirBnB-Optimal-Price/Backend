129cde778e025ec58be19f7a453badad74dfde10
Christopher Ksiazak <cksiazak@gmail.com>
2019-12-18 23:08:32 -0700

    psql not connecting, using sqlite in the meantime

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,13 @@
 require('dotenv').config();
-
-const localPg = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE
-};
+// Update with your config settings.
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: localPg,
+    client: 'sqlite3',
+    connection: {
+      filename: './database/database.db3'
+    },
+    useNullAsDefault: true,
     migrations: {
       directory: './database/migrations'
     },
@@ -18,21 +15,22 @@ module.exports = {
       directory: './database/seeds'
     }
   },
-
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/database.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
   production: {
     client: 'pg',
-    connection: process.env.DB_URL,
-    migrations: {
-      directory: './database/migrations'
-    },
-    seeds: {
-      directory: './database/seeds'
-    }
-  },
-
-  testing: {
-    client: 'pg',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations'
     },
