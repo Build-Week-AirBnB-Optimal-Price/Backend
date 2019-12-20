d69b399c506cd73f8584ca6c5d67478cf237af72
Christopher Ksiazak <cksiazak@gmail.com>
2019-12-19 21:25:14 -0700

    added documentation for registration route

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,42 @@ const genToken = require('../auth/generateToken');
 
 const User = require('../models/User-model');
 
+/**
+ * @api {post} /register Registers a user
+ * @apiName Register
+ * @apiGroup Accounts
+ *
+ * @apiparam {String} first_name User's firstname
+ * @apiparam {String} last_name User's lastname
+ * @apiparam {String} email User's email - must be unique
+ * @apiparam {String} username User's username - must be unique
+ * @apiparam {String} password User's password
+ *
+ * @apiParamExample Example Body:
+ * {
+ *    "first_name" : "Darth",
+ *    "last_name" : "Vader",
+ *    "email" : "dvader@deathstar.gov",
+ *    "username" : "xX_DarthVader_Xx",
+ *    "password" : "UnderestimatedPower"
+ * }
+ *
+ * @apiSuccess {Number} id User's account id
+ * @apisuccess {String} username User's username
+ * @apisuccess {String} token JWT generated for the user
+ *
+ * @apiSuccessExample Successful response:
+ *    HTTP/1.1 201 OK
+ * {
+ *    "user": {
+ *        "id" : 5,
+ *        "username" : "xX_DarthVader_Xx"
+ *      },
+ *    "token" : "a really long string of letters and numbers, separated by dots"
+ *
+ * }
+ */
+
 // @ROUTE       POST /register
 // @DESC        Registers a user
 // @ACCESS      PUBLIC
