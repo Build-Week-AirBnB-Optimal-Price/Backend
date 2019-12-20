a0646d73d1a58528786107ab195ab8723ef998c9
Christopher Ksiazak <cksiazak@gmail.com>
2019-12-19 21:46:31 -0700

    added static rendering to express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const express = require('express');
 const server = require('./api/server');
 
 const PORT = process.env.PORT || 5000;
@@ -6,3 +7,5 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+server.use('/', express.static('./docs'));
