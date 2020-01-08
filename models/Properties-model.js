51a0bb050a12874e8c571bb1563f2ba31514ca3e
Christopher Ksiazak <cksiazak@gmail.com>
2020-01-07 21:13:10 -0700

    added all routes needed

--- models/Properties-model.js
+++ models/Properties-model.js
@@ -3,7 +3,9 @@ const db = require('../database/db-config');
 module.exports = {
   getUserProps,
   getSpecificProp,
-  addUserProperty
+  addUserProperty,
+  updateUserProperty,
+  removeProp
 };
 
 function getUserProps(userID) {
@@ -58,6 +60,31 @@ function getSpecificProp(userID, propID) {
     .first();
 }
 
+function findById(id) {
+  return db('user_airbnb')
+    .select(
+      'id',
+      'name',
+      'host_since',
+      'zipcode',
+      'room_type',
+      'max_nights',
+      'min_nights',
+      'extra_people',
+      'accomodates',
+      'neighborhood',
+      'beds',
+      'property_type',
+      'cancel_policy',
+      'guests',
+      'bedrooms',
+      'bathrooms',
+      'optimal_price'
+    )
+    .where({ id })
+    .first();
+}
+
 function addUserProperty(prop) {
   return db('user_airbnb').insert(prop, [
     'id',
@@ -78,4 +105,40 @@ function addUserProperty(prop) {
     'bathrooms',
     'optimal_price'
   ]);
+}
+
+function updateUserProperty(userID, propID, prop) {
+  return db('user_airbnb')
+    .where({
+      host_id: userID,
+      id: propID
+    })
+    .update(prop, [
+      'id',
+      'name',
+      'host_since',
+      'zipcode',
+      'room_type',
+      'max_nights',
+      'min_nights',
+      'extra_people',
+      'accomodates',
+      'neighborhood',
+      'beds',
+      'property_type',
+      'cancel_policy',
+      'guests',
+      'bedrooms',
+      'bathrooms',
+      'optimal_price'
+    ]);
+}
+
+function removeProp(userID, propID) {
+  return db('user_airbnb')
+    .where({
+      host_id: userID,
+      id: propID
+    })
+    .del();
 }
--- routes/userProperties.js
+++ routes/userProperties.js
@@ -9,7 +9,7 @@ const Properties = require('../models/Properties-model');
 // @ROUTE       GET /user/:userID/properties
 // @DESC        Gets user's saved properties
 // @ACCESS      Private
-router.get('/:userID/properties/', async (req, res) => {
+router.get('/:userID/properties/', auth, verifyRoute, async (req, res) => {
   const { userID } = req.params;
   try {
     const userData = await Properties.getUserProps(userID);
@@ -29,28 +29,33 @@ router.get('/:userID/properties/', async (req, res) => {
 // @ROUTE       GET /user/:userID/properties/:propID
 // @DESC        Get specific saved property
 // @ACCESS      Private
-router.get('/:userID/properties/:propID', async (req, res) => {
-  const { userID, propID } = req.params;
+router.get(
+  '/:userID/properties/:propID',
+  auth,
+  verifyRoute,
+  async (req, res) => {
+    const { userID, propID } = req.params;
 
-  try {
-    const userData = await Properties.getSpecificProp(userID, propID);
+    try {
+      const userData = await Properties.getSpecificProp(userID, propID);
 
-    if (!userData) {
-      res.status(200).json({ message: 'That property does not exist' });
+      if (!userData) {
+        res.status(200).json({ message: 'That property does not exist' });
+      }
+
+      res.status(200).json(userData);
+    } catch (err) {
+      // catch errors
+      console.log(err);
+      res.status(500).json({ message: 'Server Error' });
     }
-
-    res.status(200).json(userData);
-  } catch (err) {
-    // catch errors
-    console.log(err);
-    res.status(500).json({ message: 'Server Error' });
   }
-});
+);
 
 // @ROUTE       POST /user/:userID/properties
 // @DESC        Add a property to the user's account
 // @ACCESS      Private
-router.post('/:userID/properties/', async (req, res) => {
+router.post('/:userID/properties/', auth, verifyRoute, async (req, res) => {
   const { userID } = req.params;
   const { body } = req;
   const {
@@ -112,25 +117,63 @@ router.post('/:userID/properties/', async (req, res) => {
 // @ROUTE       PUT /user/:userID/properties/:propID
 // @DESC        Update a user's info saved property info
 // @ACCESS      Private
-router.put('/:userID/properties/:propID', async (req, res) => {
-  try {
-  } catch (err) {
-    // catch errors
-    console.log(err);
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
+router.put(
+  '/:userID/properties/:propID',
+  auth,
+  verifyRoute,
+  async (req, res) => {
+    const { userID, propID } = req.params;
+    const { body } = req;
+    const userProperty = new Object({ ...body, host_id: userID });
+    try {
+      const userData = await Properties.updateUserProperty(
+        userID,
+        propID,
+        userProperty
+      );
 
-// @ROUTE       DELETE /user/:id/properties/:propID
+      if (!userData[0]) {
+        res.status(404).json({ message: 'Property does not exist' });
+      }
+
+      res.status(201).json({
+        message: 'Property has been updated',
+        user_property: userData[0]
+      });
+    } catch (err) {
+      // catch errors
+      console.log(err);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  }
+);
+
+// @ROUTE       DELETE /user/:userID/properties/:propID
 // @DESC        Gets user's information
 // @ACCESS      Private
-router.delete('/:userID/properties/:propID', async (req, res) => {
-  try {
-  } catch (err) {
-    // catch errors
-    console.log(err);
-    res.status(500).json({ message: 'Server Error' });
+router.delete(
+  '/:userID/properties/:propID',
+  auth,
+  verifyRoute,
+  async (req, res) => {
+    const { userID, propID } = req.params;
+
+    try {
+      let userData = await Properties.removeProp(userID, propID);
+
+      if (userData == 0) {
+        res.status(404).json({ message: 'Property was not found' });
+      }
+
+      res.status(200).json({
+        message: 'Property has been successfully deleted'
+      });
+    } catch (err) {
+      // catch errors
+      console.log(err);
+      res.status(500).json({ message: 'Server Error' });
+    }
   }
-});
+);
 
 module.exports = router;
